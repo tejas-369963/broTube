245d008aee5c2dbfa263053919e244b59b3d27de
Tejas-963 <tcodercpp233@gmail.com>
2025-08-17 21:30:53 +0530

    some stupid fixes

--- client/src/pages/Collections/Collections.jsx
+++ client/src/pages/Collections/Collections.jsx
@@ -7,7 +7,7 @@ function Collection() {
   return !user ? <div className='w-full h-full text-center flex flex-col justify-center items-center'><h1>Login to see </h1><h2>Collections</h2></div> 
 	: (
     <div className='w-full h-full text-center flex flex-col justify-center items-center'>
-      <h1>Site development</h1>
+      <h1>Site under development</h1>
       <h2>You will see this page soon</h2>
     </div>
   )
--- client/src/pages/Subs/Subs.jsx
+++ client/src/pages/Subs/Subs.jsx
@@ -7,7 +7,7 @@ function Subs() {
   return !user ? <div className='w-full h-full text-center flex justify-center items-center'><h1>Login to see <h2>Subscriptions</h2></h1></div> 
 	: (
     <div className='w-full h-full text-center flex flex-col justify-center items-center'>
-      <h1>Site development</h1>
+      <h1>Site under development</h1>
       <h2>You will see this page soon</h2>
     </div>
   )
